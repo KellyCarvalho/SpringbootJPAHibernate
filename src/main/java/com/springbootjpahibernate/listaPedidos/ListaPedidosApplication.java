94e3e458f0462c78e58fd0a331c869fd7b6ff34f
KellyCarvalho <18kellycarvalho@gmail.com>
2021-01-05 19:59:25 -0300

    salvando aplicação

---
+++ src/main/java/com/springbootjpahibernate/listaPedidos/ListaPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.springbootjpahibernate.listaPedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootjpahibernate/listaPedidos/ListaPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootjpahibernate.listaPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

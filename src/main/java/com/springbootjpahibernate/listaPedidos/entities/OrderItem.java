7dcc17455d0c2c41dc2b9290a8d178fe6827eefa
KellyCarvalho <18kellycarvalho@gmail.com>
2021-01-07 19:48:31 -0300

    Demais associações

---
+++ src/main/java/com/springbootjpahibernate/listaPedidos/entities/OrderItem.java
@@ -0,0 +1,56 @@
+package com.springbootjpahibernate.listaPedidos.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.springbootjpahibernate.listaPedidos.entities.pk.OrdemItemPk;
+@Entity
+@Table(name ="tb_order_item")
+public class OrderItem implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@EmbeddedId
+	private OrdemItemPk id;
+	private Integer quantity;
+	private Double price;
+	
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+	    id.setOrder(order);
+	    id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		this.setOrder(order);
+		
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void  setProduct(Product product) {
+		
+		this.setProduct(product);
+		
+	}
+
+	public OrdemItemPk getId() {
+		return id;
+	}
+
+	public void setId(OrdemItemPk id) {
+		this.id = id;
+	}
+	
+	
+
+}
